38d892305c50abdcd0cbe72e195fe69adfe5fc21
CANDICE MILLS <candice_crane_07@yahoo.com>
2018-03-25 02:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function happyHolidayTo(holiday, name)
   return "Happy " + holiday + ", " + name + "!"
 }
 
+function holidayCountdown(days, holiday)
+{
+  return "It's " + days + "days until " + holiday + "!"
+}
