35365a16f73d33bab29d5114b01f57ab43d1966e
CANDICE MILLS <candice_crane_07@yahoo.com>
2018-03-25 02:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(string)
+{
+  return "Happy holidays!"
+}
+
+var NameReturn = document.queryselector("#name")
+
+function happyHolidaysTo(string)
+{
+  return "Happy holidays, " 
+  NameReturn.innerhtml
+  return "!"
+}
